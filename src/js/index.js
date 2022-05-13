e03e596855765366855f1d4f7c2fd8624536b65a
solmazfs <info.solmazfs@gmail.com>
2022-05-13 22:38:34 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
 import { Grid } from './grid';
 const cursor = require('./cursor');
 
-var ghpages = require('gh-pages');
-ghpages.publish('dist', function(err) {});
-
 document.addEventListener("DOMContentLoaded", function(event) {
     window.onload = function () {
         document.body.classList.remove('loading');
